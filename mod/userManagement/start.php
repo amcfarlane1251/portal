05e334434adf84bcfe8ba860e9e59f38f2067c47
Alex McFarlane <amcfarlane1251@gmail.com>
2015-09-23 13:09:55 -0400

    usermgmt: admin activate

--- mod/userManagement/start.php
+++ mod/userManagement/start.php
@@ -11,6 +11,7 @@ function userManagementInit()
 	elgg_register_action('changeEmail', $pluginPath.'actions/changeEmail.php', 'public');
 	elgg_register_action('resetPassword', $pluginPath.'actions/resetPassword.php', 'public');
 	elgg_register_action('users/import', $pluginPath."actions/import.php", 'admin');
+	elgg_register_action('admin/activate', $pluginPath."actions/admin/activate.php", 'admin');
 	//register page handler for routes
 	elgg_register_page_handler('usermgmt', 'userManagementPageHandler');
 }
@@ -91,7 +92,11 @@ function userManagementPageHandler($page)
 		case 'registerEmails':
 			include(elgg_get_plugins_path()."userManagement/pages/registerEmails.php");
 			break;
-
+		case 'admin':
+			if($page[1] == activate) {
+				include(elgg_get_plugins_path()."userManagement/pages/admin/activate.php");
+			}
+			break;
 		default:
 			return false;
 	}
---
+++ mod/userManagement/views/default/forms/admin/activate.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<div>
+	<label class="normal"><?php echo elgg_echo('activate:email');?></label>
+	<input type="text" name="email" value="<?php echo get_input('email');?>" class="text-input"/>
+</div>
+<input type="hidden" name="guid" value="<?php echo get_input('guid');?>" class="hidden"/>
+<input type="submit" name="submit" class="elgg-button elgg-button-submit" value="Activate"/>
