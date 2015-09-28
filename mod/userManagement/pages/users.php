d81e3a002903c1516b274a913e606736a6b38af6
Alex McFarlane <amcfarlane1251@gmail.com>
2015-09-28 09:35:44 -0400

    usermgmt: admin activate users

--- mod/userManagement/pages/users.php
+++ mod/userManagement/pages/users.php
@@ -1,2 +1,18 @@
 <?php
-//$userMgmt = UserManagement::withFilter()
+/* TODO: User query string
+ * $_SERVER['QUERY_STRING'];
+ */
+$deactivated = $_GET['deactivated'];
+
+$userMgmt = new UserManagement();
+$users = $userMgmt->getUsers($deactivated);
+$obj = array();
+foreach($users as $user) {
+	$userArr = array(
+		'guid' => $user->guid,
+		'name' => $user->name,
+	);
+	array_push($obj, $userArr);
+}
+
+echo json_encode($obj);
--- mod/userManagement/views/default/forms/admin/activate.php
+++ mod/userManagement/views/default/forms/admin/activate.php
@@ -2,33 +2,61 @@
 
 ?>
 <div>
-	<label class="normal">Add User: </label>
+	<label class="normal">User: </label>
 	<input type="text" name="user" id="name" class="text-input"/>
 </div>
 <input type="submit" name="submit" class="elgg-button elgg-button-submit" value="Activate"/>
 
 <script>
   $(function() {
-    var projects = $.ajax({
-    				   url:'usermgmt/users?deactivated=true'
-    			   });
- 
     $( "#name" ).autocomplete({
-      minLength: 0,
-      source: projects,
+      source: function(request, response) {
+        var results = [];
+        var term = request.term;
+        $.ajax({
+          url:'../../usermgmt/users?deactivated=true',
+          success:function(data){
+            if(term.length > 0) {
+              var users = JSON.parse(data);
+              for(var index in users) {
+                if(users[index]['name'].toLowerCase().indexOf(term.toLowerCase()) == 0) {
+                  results.push(users[index]);
+                }
+              } 
+            }
+            else{
+              results = ["Start typing..."];
+            }
+
+            response($.map(results, function(item){
+              if(item['guid']) {
+                return {
+                  label: item['name'],
+                  value: item['name'],
+                  guid: item['guid']
+                }
+              }
+              else{
+                return results;
+              }
+            }));
+          }
+        });
+      },
+      minLength: 1,
       focus: function( event, ui ) {
-        $( "#name" ).val( ui.item.label );
+        $( "#name" ).val( ui.item.name );
         return false;
       },
       select: function( event, ui ) {
-        $( "#name" ).val( ui.item.label );
+        $( "#name" ).val( ui.item.name );
         return false;
       }
     })
     .autocomplete( "instance" )._renderItem = function( ul, item ) {
       return $( "<li>" )
-        .append( "<a>" + item.label + "<br>" + item.desc + "</a>" )
+        .append( "<a>" + item.name + "<br>" + item.guid + "</a>" )
         .appendTo( ul );
     };
   });
-  </script>
+</script>
