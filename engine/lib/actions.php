c022058febf3588dabbfbee405d80ba0c0606e6f
justinward25 <wjustin1994@gmail.com>
2015-10-01 09:27:45 -0400

    reverted changes in action.php

--- engine/lib/actions.php
+++ engine/lib/actions.php
@@ -225,10 +225,6 @@ function _elgg_validate_token_timestamp($ts) {
  */
 function validate_action_token($visibleerrors = TRUE, $token = NULL, $ts = NULL) {
 
-	$tokenArr = [];
-	$check = true;
-	echo $tokenArr;
-
 	if (!$token) {
 		$token = get_input('__elgg_token');
 	}
@@ -246,13 +242,6 @@ function validate_action_token($visibleerrors = TRUE, $token = NULL, $ts = NULL)
 		// Validate token
 		if ($token == $required_token) {
 			
-			foreach($tokenArr as $array) {
-				if($token = $array) {
-					$check = false;
-					break;
-				}
-			}
-
 			if (_elgg_validate_token_timestamp($ts) && $check == true) {
 				// We have already got this far, so unless anything
 				// else says something to the contrary we assume we're ok
@@ -264,7 +253,6 @@ function validate_action_token($visibleerrors = TRUE, $token = NULL, $ts = NULL)
 				), $returnval);
 
 				if ($returnval) {
-					$tokenArr[] = $token;
 					return true;
 				} else if ($visibleerrors) {
 					register_error(elgg_echo('actiongatekeeper:pluginprevents'));
