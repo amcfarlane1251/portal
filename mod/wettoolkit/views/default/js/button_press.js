7ce2f549bf18a309295d53f31574f948a2092180
justinward25 <wjustin1994@gmail.com>
2015-10-07 09:06:04 -0400

    changed compatability check by checking for XMLHttp instead

--- mod/wettoolkit/views/default/js/button_press.js
+++ mod/wettoolkit/views/default/js/button_press.js
@@ -9,14 +9,8 @@ $(document).ready(function() {
 
         //only display progress bar if file to upload
         if(fileInput.val()) {
-            //checks for IE browser version less than 10
-            if($.browser.msie && parseInt($.browser.version, 10) < 10) {
-                this.ajaxLoader = elgg.normalize_url("/_graphics/ajax_loader.gif");
-                var spinner = "<div class='ajax-spinner'>" + "<img id='file-upload-spinner' src='"+this.ajaxLoader+"' alt='Loading Content...' + />" + "</div>" + 
-                    "<div id='upload-text'>" + "<p>Uploading File...</p>" + "</div>";
-                $(this).append(spinner).fadeIn(100);
-            } 
-            else {
+            //check for XMLHttpRequest support
+            if(XMLHttpRequest) {
                 var form = document.getElementById("portalForm");
                 var fd = new FormData(form);
 
@@ -26,6 +20,12 @@ $(document).ready(function() {
 
                 xhr.open("POST", event);
                 xhr.send(fd);
+            } 
+            else {
+                this.ajaxLoader = elgg.normalize_url("/_graphics/ajax_loader.gif");
+                var spinner = "<div class='ajax-spinner'>" + "<img id='file-upload-spinner' src='"+this.ajaxLoader+"' alt='Loading Content...' + />" + "</div>" + 
+                    "<div id='upload-text'>" + "<p>Uploading File...</p>" + "</div>";
+                $(this).append(spinner).fadeIn(100);
             }
         }
     });
@@ -38,5 +38,17 @@ function uploadProgress(evt) {
     $('#progressLabel').text(percentComplete + "%");
     $('#progressBar').show("fast");
     $('#progressBar').attr('value', percentComplete);
+}
+
+function ieVersion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf ( "MSIE " );
+
+    if ( msie > 0 ) { 
+        return parseInt (ua.substring (msie+5, ua.indexOf (".", msie )));
+    }
+    else {      
+        return 0;
+    }
 }
 
