0c72a5d81264090a96001ddc21c007dd7108911c
FEI WANG <kingnot0524@gmail.com>
2016-01-21 11:55:46 -0500

    Some bugs fixed for joyride display

--- mod/groups/views/default/groups/profile/summary.php
+++ mod/groups/views/default/groups/profile/summary.php
@@ -72,6 +72,14 @@ if ($group->canEdit()) {
 	        <h2>".elgg_echo('groups:menu:copy:tooltipTitle')."</h2>
 	        <p>".elgg_echo('groups:menu:copy:tooltip')."</p>
 	    </li>";
+    //if the admin is not the member of the current group, show tooltip for Join button
+    if (!$group->isMember(elgg_get_logged_in_user_entity())) {
+    	echo "
+    	<li data-class='elgg-menu-item-groups-join' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+        	<h2>".elgg_echo('groups:menu:join:tooltipTitle')."</h2>
+        	<p>".elgg_echo('groups:menu:join:tooltip')."</p>
+    	</li>";
+    }
 }
 //if the user is the member of the current group
 if ($group->isMember(elgg_get_logged_in_user_entity())) {
@@ -82,36 +90,14 @@ if ($group->isMember(elgg_get_logged_in_user_entity())) {
 	        <h2>".elgg_echo('groups:menu:leave:tooltipTitle')."</h2>
 	        <p>".elgg_echo('groups:menu:leave:tooltip')."</p>
 	    </li>";
-	    //if the user is the admin, show tooltip for Join button
-	    if ($group->canEdit()) {
-	    	echo "
-	    	<li data-class='elgg-menu-item-groups-join' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-	        	<h2>".elgg_echo('groups:menu:join:tooltipTitle')."</h2>
-	        	<p>".elgg_echo('groups:menu:join:tooltip')."</p>
-	    	</li>";
-	    }
-	    //else if the user is just the group member
-	    else {
-	    	//if group members can invite by settings, show tooltip for Invite button
-	    	if ($group->invite_members) {
-		    echo "
-		    <li data-class='elgg-menu-item-groups-invite' data-text='".elgg_echo('widget_manager:widgets:next')."' data-prev-text='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:bottom;tipAnimation:fade'>
-		        <h2>".elgg_echo('groups:menu:invite:tooltipTitle')."</h2>
-		        <p>".elgg_echo('groups:menu:invite:tooltip')."</p>
-	    	</li>";
-    		}
-	    }
-	}
-	//if subgroups are allowed by settings
-	if ($group->subgroups_enable == "yes") {
-		//if the user is admin or group owner,
-		//or the group allows subgroups and allows any member to create subgroups, show tooltip for Subgroup
-		if ($group->canEdit() || $group->subgroups_members_create_enable == 'yes') {
-			echo "
-	    	<li data-class='elgg-menu-item-add-subgroup' data-text='".elgg_echo('widget_manager:widgets:next')."' data-prev-text='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:bottom;tipAnimation:fade'>
-	        	<h2>".elgg_echo('groups:menu:subgroup:tooltipTitle')."</h2>
-	        	<p>".elgg_echo('groups:menu:subgroup:tooltip')."</p>
-	    	</li>";
+	    //if the user is not the admin,
+    	//and group members can invite by settings, show tooltip for Invite button
+    	if (!$group->canEdit() && $group->invite_members) {
+	    echo "
+	    <li data-class='elgg-menu-item-groups-invite' data-text='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:bottom;tipAnimation:fade'>
+	        <h2>".elgg_echo('groups:menu:invite:tooltipTitle')."</h2>
+	        <p>".elgg_echo('groups:menu:invite:tooltip')."</p>
+    	</li>";
 		}
 	}
 }
@@ -127,11 +113,25 @@ elseif (elgg_is_logged_in()) {
 	}
 	//if this is a ClOSED group, show the tooltip for REQUEST button
 	else {
+		if (!$group->canEdit()) {
+			echo "
+		    <li data-class='elgg-menu-item-groups-joinrequest' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+		        <h2>".elgg_echo('groups:menu:joinrequest:tooltipTitle')."</h2>
+		        <p>".elgg_echo('groups:menu:joinrequest:tooltip')."</p>
+		    </li>";
+		}
+	}
+}
+//if subgroups are allowed by settings
+if ($group->subgroups_enable == "yes") {
+	//if the user is admin or group owner,
+	//or the group allows subgroups and allows any member to create subgroups, show tooltip for Subgroup
+	if ($group->canEdit() || ($group->isMember(elgg_get_logged_in_user_entity()) && $group->subgroups_members_create_enable == 'yes')) {
 		echo "
-	    <li data-class='elgg-menu-item-groups-joinrequest' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-	        <h2>".elgg_echo('groups:menu:joinrequest:tooltipTitle')."</h2>
-	        <p>".elgg_echo('groups:menu:joinrequest:tooltip')."</p>
-	    </li>";
+    	<li data-class='elgg-menu-item-add-subgroup' data-text='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	        <h2>".elgg_echo('groups:menu:subgroup:tooltipTitle')."</h2>
+	        <p>".elgg_echo('groups:menu:subgroup:tooltip')."</p>
+		</li>";
 	}
 }
 
@@ -148,12 +148,15 @@ echo "
 /*
  *	Listing of enabled group plug-ins
  */
-echo "
-<!-- Tooltip for Group Plug-ins -->
-<li data-id='groups-tools' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:right;tipAnimation:fade'>
-    <h2>".elgg_echo('groups:tools:tooltipTitle')."</h2>
-    <p>".elgg_echo('groups:tools:tooltip')."</p>
-</li>";
+//If user is in the group, or user is admin, show the tooltip for group plug-ins
+if ($group->isMember(elgg_get_logged_in_user_entity()) || $group->canEdit()) {
+	echo "
+	<!-- Tooltip for Group Plug-ins -->
+	<li data-id='groups-tools' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:right;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:tools:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:tools:tooltip')."</p>
+	</li>";
+}
 
 /*
  *	Icons
@@ -170,45 +173,58 @@ echo "
     <p>".elgg_echo('groups:icon:rss:tooltip')."</p>
 </li>";
 
+//If user is not in the group, nor user is admin, show the tooltips for Sub-group List, Search Box & Members List
+if (!$group->isMember(elgg_get_logged_in_user_entity()) && !$group->canEdit()) {
 /*
  *	Sidebar
  */
-echo "
-<!-- Tooltip for Sidebar -->
-<li data-class='elgg-menu-owner-block' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-    <h2>".elgg_echo('groups:menu:sidebar:tooltipTitle')."</h2>
-    <p>".elgg_echo('groups:menu:sidebar:tooltip')."</p>
-</li>";
-
+	echo "
+	<!-- Tooltip for Sidebar -->
+	<li data-class='elgg-menu-owner-block' data-button='".elgg_echo('widget_manager:widgets:close')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:menu:sidebar:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:menu:sidebar:tooltip')."</p>
+	</li>";
+}
+else {
+/*
+ *	Sidebar
+ */
+	echo "
+	<!-- Tooltip for Sidebar -->
+	<li data-class='elgg-menu-owner-block' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:menu:sidebar:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:menu:sidebar:tooltip')."</p>
+	</li>";
 /*
  *	Sub-group List
  */
-echo "
-<!-- Tooltip for Sub-group List -->
-<li data-class='elgg-module-aside' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-    <h2>".elgg_echo('groups:module:subgroup:tooltipTitle')."</h2>
-    <p>".elgg_echo('groups:module:subgroup:tooltip')."</p>
-</li>";
+	echo "
+	<!-- Tooltip for Sub-group List -->
+	<li data-class='elgg-module-aside' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:module:subgroup:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:module:subgroup:tooltip')."</p>
+	</li>";
 
 /*
  *	Search Box
  */
-echo "
-<!-- Tooltip for Search Box -->
-<li data-class='elgg-form-groups-search' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-    <h2>".elgg_echo('groups:form:search:tooltipTitle')."</h2>
-    <p>".elgg_echo('groups:form:search:tooltip')."</p>
-</li>";
+	echo "
+	<!-- Tooltip for Search Box -->
+	<li data-class='elgg-form-groups-search' data-button='".elgg_echo('widget_manager:widgets:next')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:form:search:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:form:search:tooltip')."</p>
+	</li>";
 
 /*
  *	Members List
  */
-echo "
-<!-- Tooltip for Members List -->
-<li data-class='elgg-gallery-users' data-button='".elgg_echo('widget_manager:widgets:close')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
-    <h2>".elgg_echo('groups:gallery:users:tooltipTitle')."</h2>
-    <p>".elgg_echo('groups:gallery:users:tooltip')."</p>
-</li>";
+	echo "
+	<!-- Tooltip for Members List -->
+	<li data-class='elgg-gallery-users' data-button='".elgg_echo('widget_manager:widgets:close')."' data-prev-button='".elgg_echo('widget_manager:widgets:prev')."' data-options='tipLocation:left;tipAnimation:fade'>
+	    <h2>".elgg_echo('groups:gallery:users:tooltipTitle')."</h2>
+	    <p>".elgg_echo('groups:gallery:users:tooltip')."</p>
+	</li>";	
+}
 
 echo "
 </ol>";
